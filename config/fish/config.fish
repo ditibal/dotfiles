c8600bd5841eaa76e2be72b491be2529251f48d9
ditibal <ditibal@gmail.com>
2021-03-17 15:39:02 +0300

    добавлены конфиги для fish

---
+++ config/fish/config.fish
@@ -0,0 +1,40 @@
+set -gx PATH $PATH ~/bin
+set -gx TERM xterm-256color
+set -gx EDITOR xvim
+set -gx GOPATH ~/go
+set -gx XDG_CACHE_HOME ~/.cache
+set -gx CHEATCOLORS true
+set -gx NVIM_LISTEN_ADDRESS /tmp/nvimsocket
+
+function r
+    alias r "ranger"
+    if not set -q RANGER_LEVEL
+        ranger
+    else
+        exit
+    end
+end
+
+alias l "ls --color=auto -lh"
+alias sp "sudo pacman"
+alias rm "rm -i"
+alias gs "git status "
+alias ga "git add "
+alias gb "git branch "
+alias gc "git commit"
+alias gd "git diff"
+alias gito "git checkout "
+alias gk "gitk --all&"
+alias gx "gitx --all"
+alias crontab "set -l VISUAL vim; set -l EDITOR vim; /usr/bin/crontab"
+alias v "vagrant"
+alias vssh "vagrant ssh"
+alias wtmux "tmux has-session -t work >> /dev/null 2>&1 ; and tmux attach-session -t work; or tmux new-session -s work"
+
+alias gulp-coffee="gulp --require coffee-script/register"
+alias btb="lpass show 3113225606 | grep Password: | cut -d ' ' -f 2 | tr -d ' \t\n\r' | xclip"
+alias awesome-restart="echo 'awesome.restart()' | awesome-client"
+
+alias reboot "sudo systemctl reboot"
+alias poweroff "sudo systemctl poweroff"
+alias halt "sudo systemctl halt"
