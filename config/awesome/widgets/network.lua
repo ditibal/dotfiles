8c5f6e2cb6565901bb05c7601c12f2a8751fde6c
ditibal <ditibal@gmail.com>
2022-05-31 18:16:08 +0300

    awesome: изменено имя сетевого интерфейса

--- config/awesome/widgets/network.lua
+++ config/awesome/widgets/network.lua
@@ -7,8 +7,8 @@ local netup_icon = wibox.widget.imagebox(theme.net_up)
 netwidget = wibox.widget.textbox()
 vicious.cache(vicious.widgets.net)
 vicious.register(netwidget, vicious.widgets.net,
-    '<span color="#CC9393">${enp3s0 down_kb}</span>' ..
-        ' <span color="#7F9F7F">${enp3s0 up_kb}</span>', 2)
+    '<span color="#CC9393">${enp4s0 down_kb}</span>' ..
+        ' <span color="#7F9F7F">${enp4s0 up_kb}</span>', 2)
 
 local networkwidget = wibox.container.margin(netwidget, 0, 0, 5, 5)
 
