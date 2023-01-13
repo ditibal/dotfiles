bb7cc11f294b1bab450fb1ae76fcc5665f6e23c4
ditibal <ditibal@gmail.com>
2023-01-14 02:33:22 +0300

    awesome: добавлено управление громкостью мышью

--- config/awesome/widgets/volume.lua
+++ config/awesome/widgets/volume.lua
@@ -1,6 +1,7 @@
 local lain = require("lain")
 local wibox = require("wibox")
 local gears = require("gears")
+local awful = require("awful")
 
 volume = lain.widget.alsabar({
     notification_preset = { font = "Monospace 9" },
@@ -17,6 +18,20 @@ volume = lain.widget.alsabar({
 
 volume.bar.paddings = 0
 volume.bar.margins = 5
+
+volume.bar:buttons (awful.util.table.join (
+        awful.button ({}, 1, function()
+            awful.spawn(string.format("amixer set %s toggle", 'Master'))
+            volume.update()
+        end),
+        awful.button ({}, 4, function()
+            amixer_set('1%+')
+        end),
+        awful.button ({}, 5, function()
+            amixer_set('1%-')
+        end)
+))
+
 local volumewidget = wibox.container.background(volume.bar, theme.bg_focus, gears.shape.rectangle)
 volumewidget = wibox.container.margin(volumewidget, 0, 0, 5, 5)
 
