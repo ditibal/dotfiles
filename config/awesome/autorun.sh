5f8a42d2dc4d57347ceb2c0d90514795f3c63544
ditibal <ditibal@gmail.com>
2023-11-16 10:56:07 +0300

    awesome: обновлен автозапуск

--- config/awesome/autorun.sh
+++ config/awesome/autorun.sh
@@ -9,3 +9,11 @@ run() {
 run "setxkbmap -layout us,ru -variant -option grp:caps_toggle"
 run "autocutsel -s PRIMARY"
 run "autocutsel -s CLIPBOARD"
+
+if [ "$HOSTNAME" = "laptop" ]; then
+    run "xfce4-power-manager --daemon"
+fi
+
+if [ "$HOSTNAME" = "comp" ]; then
+    run "xset dpms 0 0 0"
+fi
