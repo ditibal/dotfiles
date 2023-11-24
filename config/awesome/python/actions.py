35a2231f04dd37d4ba24ef58e14232be577574f6
ditibal <ditibal@gmail.com>
2023-11-24 22:31:21 +0300

    awesome: добавлена команда для редактирования /etc/hosts

--- config/awesome/python/actions.py
+++ config/awesome/python/actions.py
@@ -9,6 +9,9 @@ def awesome_quit():
     os.system("awesome-client 'awesome.quit()'")
 
 
+def edit_hosts():
+    os.system("alacritty -e /bin/nvim /etc/hosts")
+
 actions = [
     {
         'label': 'Restart Awesome',
@@ -17,6 +20,10 @@ actions = [
     {
         'label': 'Quit',
         'action': awesome_quit,
+    },
+    {
+        'label': 'Edit hosts',
+        'action': edit_hosts,
     }
 ]
 
@@ -33,7 +40,7 @@ def run_action(index):
 def show():
     r = Rofi()
     options = [action['label'] for action in actions]
-    index, key = r.select('Run', options)
+    index, key = r.select('Run', options, rofi_args=['-i'])
     run_action(index)
 
 show()
